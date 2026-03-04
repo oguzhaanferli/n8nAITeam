639a402c901e3bb10a95720f42e25c5eef9be547
Oğuzhan Ali Ferli <oguzhaanferli@gmail.com>
2026-03-04 10:32:10 +0300

    Update src/routes/index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,24 @@
 import { Route, Routes } from 'react-router-dom'
-import Login from '@/pages/Login'
-import Register from '@/pages/Register'
-import Dashboard from '@/pages/Dashboard'
-import Tasks from '@/pages/Tasks'
-import TaskDetail from '@/pages/TaskDetail'
-import TaskForm from '@/pages/TaskForm'
-import Settings from '@/pages/Settings'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+import Dashboard from '../pages/Dashboard'
+import Tasks from '../pages/Tasks'
+import TaskDetail from '../pages/TaskDetail'
+import TaskForm from '../pages/TaskForm'
+import Settings from '../pages/Settings'
 
-export const routes = (
+const routes = (
   <Routes>
     <Route key="login" path="/login" element={<Login />} />
     <Route key="register" path="/register" element={<Register />} />
     <Route key="dashboard" path="/" element={<Dashboard />} />
     <Route key="tasks" path="/tasks" element={<Tasks />} />
     <Route key="task-detail" path="/tasks/:id" element={<TaskDetail />} />
-    <Route key="task-create" path="/tasks/new" element={<TaskForm />} />
-    <Route key="task-edit" path="/tasks/edit/:id" element={<TaskForm />} />
+    <Route key="task-create" path="/tasks/create" element={<TaskForm />} />
+    <Route key="task-edit" path="/tasks/:id/edit" element={<TaskForm />} />
     <Route key="settings" path="/settings" element={<Settings />} />
   </Routes>
 )
+
+export default routes
 ```
