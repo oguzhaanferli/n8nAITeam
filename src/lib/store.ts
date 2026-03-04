6400d7d7b00326f995e84492645c97939c0f142a
Oğuzhan Ali Ferli <oguzhaanferli@gmail.com>
2026-03-04 10:57:33 +0300

    Update src/lib/store.ts

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,12 +1,12 @@
-import { create } from 'zustand';
-import { AuthSession } from './types';
+import { create } from 'zustand'
+import { AuthSession } from './types'
 
 type AuthState = AuthSession & {
-  setSession: (session: AuthSession | null) => void;
-};
+  setSession: (session: AuthSession | null) => void
+}
 
 export const useAuthStore = create<AuthState>((set) => ({
   session: null,
-  setSession: (session: AuthSession | null) => set({ session }),
-}));
+  setSession: (session) => set({ session })
+}))
 ```
